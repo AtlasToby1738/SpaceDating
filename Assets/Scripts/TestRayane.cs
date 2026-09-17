cbd02a63c8be3ce4296c92171c09cd6f9ebb4211
Rayane-Grich <rayanegrichpro@gmail.com>
2026-09-17 11:51:52 +0200

    Dissimulation des lettres (test)

--- Assets/Scripts/TestRayane.cs
+++ Assets/Scripts/TestRayane.cs
@@ -1,16 +1,31 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestRayane : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public TMP_Text testText;
+    public Image testImagePrefab;
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        testText.ForceMeshUpdate();
+        Vector3[] verticeArray;
+        verticeArray = testText.mesh.vertices;
+        Debug.Log(verticeArray.Length);
+        Debug.Log(testText.text.Length);
+        int spaceDetection = 0;
+        for (int Increment = 0; Increment < testText.text.Length - spaceDetection; Increment++)
+        {
+            if (testText.textInfo.characterInfo[Increment].character == ' ') spaceDetection++;
+            Image testImage = Instantiate<Image>(testImagePrefab);
+            testImage.transform.SetParent(testText.transform, false);
+            testImage.rectTransform.position = (verticeArray[Increment * 4] + verticeArray[Increment * 4 + 1]) / 2 + testText.transform.position;
+            Debug.Log(Increment * 4);
+            Debug.Log(Increment * 4 + 1);
+
+            RectTransform testRect;
+            //testText.textInfo.characterInfo[Increment].
+        }
     }
 }
