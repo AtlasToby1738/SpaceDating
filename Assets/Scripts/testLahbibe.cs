12e0aa3ba9a5c11f6ccd98ab97bcc770acda730f
AtlasToby1738 <lahbibebarone2@gmail.com>
2026-09-18 14:59:29 +0200

    Coming to the main scene

--- Assets/Scripts/testLahbibe.cs
+++ Assets/Scripts/testLahbibe.cs
@@ -58,7 +58,7 @@ public class testLahbibe : MonoBehaviour
     [SerializeField] private Camera cam;
     [SerializeField] float timeDelay = 1f;
 
-    static Action<int> Score;
+    static event Action<int> Score;
 
     void Start()
     {
