f71f885c00478ffa0d2e4423c742790ad8acbb4b
Lolahahahahaha <lola.joigneaux@gmail.com>
2026-09-18 11:34:28 +0200

    finished the shader and script for the score

---
+++ Assets/VFX/Scripts/ScoreFluid.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ScoreFluid : MonoBehaviour
+{
+    [SerializeField]
+    private float duration = 1.0f;
+    [SerializeField]
+    private Material fluidMaterial;
+    [SerializeField]
+    private float maxScore;
+    [SerializeField]
+    private float length;
+    
+    private AnimationCurve curve;
+
+    private float _currentScore = 0;
+    private float _scoreFraction;
+    void OnEnable()
+    {
+        _scoreFraction = length / maxScore;
+    }
+
+    public IEnumerator IncreasingScore()
+    {
+        float _age = 0;
+        curve = AnimationCurve.Linear(0, _currentScore, 1, _currentScore + _scoreFraction);
+        
+        while (_age<1)
+        {
+            _age += Time.deltaTime / duration;
+            fluidMaterial.SetFloat("_Score", curve.Evaluate(_age));
+        yield return null;
+        }
+        _currentScore += _scoreFraction;
+    }
+    public IEnumerator DecreasingScore()
+    {
+        float _age = 0;
+        curve = AnimationCurve.Linear(0, _currentScore, 1, _currentScore - _scoreFraction);
+
+        while (_age < 1)
+        {
+            _age += Time.deltaTime / duration;
+            fluidMaterial.SetFloat("_Score", curve.Evaluate(_age));
+            yield return null;
+        }
+        _currentScore += _scoreFraction;
+    }
+}
