6a1ca401031195daa27aaa0c7e1017b681e97df3
Rayane-Grich <rayanegrichpro@gmail.com>
2026-09-18 12:13:19 +0200

    Niveau de fin fonctionnel

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -5,24 +5,29 @@ using UnityEngine.UI;
 public class EndScreen : MonoBehaviour
 {
     [SerializeField] private Image[] characterImages;
+    [SerializeField] private Image[] heartImages;
     [SerializeField] private Image player;
     [SerializeField] private Sprite failedPlayerSprite;
-    [SerializeField] private TMP_Text text;
+    [SerializeField] private Sprite heartSprite;
+    [SerializeField] private TMP_Text[] scoreTexts;
     [SerializeField] private CharacterAttributes[] attributes;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        bool isAlone = true;
+        int playerScore = 0;
         for(int Increment = 0; Increment < attributes.Length; Increment++)
         {
+            scoreTexts[Increment].text = attributes[Increment].score.ToString();
             if (attributes[Increment].score >= 0)
             {
-                characterImages[Increment].enabled = true;
-                isAlone = false;
+                characterImages[Increment].gameObject.SetActive(true);
+                heartImages[Increment].sprite = heartSprite;
+                playerScore++;
             }
         }
-        if(isAlone)
+        scoreTexts[scoreTexts.Length - 1].text = playerScore.ToString() + " / " + attributes.Length.ToString();
+        if(playerScore == 0)
         {
             player.sprite = failedPlayerSprite;
         }
